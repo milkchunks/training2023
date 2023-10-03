60b32f332cfd2c81937deed26fc462e8919db296
Allison <cattim569@gmail.com>
2023-10-03 15:42:18 -0700

    switch statement

--- src/ontopic/week1/Trapezoid.java
+++ src/ontopic/week1/Trapezoid.java
@@ -6,40 +6,44 @@ public class Trapezoid {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Which value would you like to calculate? (a/b/h)");
         String choice = scanner.next();
-        if (choice.equals("a")) {
-            System.out.println("Input area");
-            double area = scanner.nextDouble();
-            System.out.println("Input 'b' value");
-            double b = scanner.nextDouble();
-            System.out.println("Input height");
-            double height = scanner.nextDouble();
-            System.out.println(findAValue(area, b, height));
-        } else if (choice.equals("b")) {
-            System.out.println("Input area");
-            double area = scanner.nextDouble();
-            System.out.println("Input 'a' value");
-            double a = scanner.nextDouble();
-            System.out.println("Input height");
-            double height = scanner.nextDouble();
-            System.out.println(findBValue(area, a, height));
-        } else if (choice.equals("h")) {
-            System.out.println("Input area");
-            double area = scanner.nextDouble();
-            System.out.println("Input 'a' value");
-            double a = scanner.nextDouble();
-            System.out.println("Input 'b' value");
-            double b = scanner.nextDouble();
-            System.out.println(findHeight(area, a, b));
+        switch (choice) {
+            case "a": {
+                System.out.println("Input area");
+                double area = scanner.nextDouble();
+                System.out.println("Input 'b' value");
+                double b = scanner.nextDouble();
+                System.out.println("Input height");
+                double height = scanner.nextDouble();
+                System.out.println(findAValue(area, b, height));
+            }
+            case "b": {
+                System.out.println("Input area");
+                double area = scanner.nextDouble();
+                System.out.println("Input 'a' value");
+                double a = scanner.nextDouble();
+                System.out.println("Input height");
+                double height = scanner.nextDouble();
+                System.out.println(findBValue(area, a, height));
+            }
+            case "h": {
+                System.out.println("Input area");
+                double area = scanner.nextDouble();
+                System.out.println("Input 'a' value");
+                double a = scanner.nextDouble();
+                System.out.println("Input 'b' value");
+                double b = scanner.nextDouble();
+                System.out.println(findHeight(area, a, b));
+            }
         }
         scanner.close();
     }
     public static double findAValue(double area, double b, double height) {
         return 2 * (area - height) - b;
-    };
+    }
     public static double findBValue(double area, double a, double height) {
         return 2 * (area - height) - a;
     }
     public static double findHeight(double area, double a, double b) {
         return 2 * (area / (a + b));
-    };
+    }
 }
